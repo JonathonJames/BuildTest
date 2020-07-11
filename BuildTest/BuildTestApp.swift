b91e97727db8a940d97d0b70b8d9bc30f6d20f77
Jonathon James <jonathonjames@gmail.com>
2020-07-11 10:38:09 +0100

    Initial Commit

---
+++ BuildTest/BuildTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTestApp.swift
+//  BuildTest
+//
+//  Created by Jonathon James on 11/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct BuildTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
